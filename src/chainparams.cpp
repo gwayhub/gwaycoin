6eb398f53d15e3e23406fc3b28d068e45547b87f
gwaycoin <gwaycoin@gwaycoin.org>
2019-05-30 11:17:42 +0300

    Set the best chain work parameter based on the current difficulty

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -121,7 +121,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_DIP0001].nThreshold = 3226; // 80% of 4032
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x00"); // unknown yet
+        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000006000000000");
 
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x00"); // unknown yet
